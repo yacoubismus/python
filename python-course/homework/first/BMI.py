87b1a9b9879f197e5ce54f6978e2cdaf4c900ea4
Jacob <yacoubium@yahoo.de>
2016-10-24 21:47:56 +0200

    BMI impl

--- python-course/homework/first/BMI.py
+++ python-course/homework/first/BMI.py
@@ -3,3 +3,19 @@ Created on 24.10.2016
 
 @author: yacoub
 '''
+from BMI_Validators import *
+while True:
+    print ('We will count your BMI.')
+    weight = input("Your weight in KG please !")
+    if (validate_weight()):
+        print ("Invalid weight")
+        break
+    height = input("Your height in Meter please !")
+    if(validate_height(height)):
+        print ("Invalid height")
+        break
+    bmi = weight / (height**2)
+    print("Your BMI is %s"% bmi.__str__())
+
+
+
