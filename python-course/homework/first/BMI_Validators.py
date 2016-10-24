2cfdeca5e5cf89448b41cad7a651c96a50461080
Jacob <yacoubium@yahoo.de>
2016-10-24 21:09:39 +0200

    validatore height weight

---
+++ python-course/homework/first/BMI_Validators.py
@@ -0,0 +1,13 @@
+def validate_weight(weight):
+    if weight > 200 or weight < 10:
+        return False
+    else:
+        return True
+
+
+
+def validate_height(height):
+    if height > 2.5:
+        return False
+    else:
+        return True
