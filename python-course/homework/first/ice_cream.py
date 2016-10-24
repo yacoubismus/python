74575076fc6d237932aabef7b1374b9ebde2b276
Jacob <yacoubium@yahoo.de>
2016-10-24 20:46:49 +0200

    ice cream

--- python-course/homework/first/ice_cream.py
+++ python-course/homework/first/ice_cream.py
@@ -4,3 +4,12 @@ Created on 24.10.2016
 @author: yacoub
 '''
 ball_price = 0.9
+
+for index  in range(1, 21):
+    ball = ''
+    if index ==1:
+        ball = 'ball'
+    else:
+        ball = 'balls'
+    print ("Price of %s %s is %1.1f Euro" % (index, ball ,round(index * ball_price ,1)))
+
