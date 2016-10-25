258ad9731e1a7dbff9f80de346aa05949fe839ec
yacoub <yacoubismus@googlemail.com>
2016-10-25 15:35:40 +0200

    primary

--- python-course/homework/first/Sieve_Erathostenes.py
+++ python-course/homework/first/Sieve_Erathostenes.py
@@ -4,15 +4,23 @@ Created on 25.10.2016
 @author: yacoub
 '''
 def sieve_erathostens(max):
-    primaries = [2]
-    for number in range(max):
-        
-        for num in range(3, number):
-            flag = True
+    primaries = [2,3]
+    for number in range(4,max):
+        flag = True
+        for num in range(2, number):
+            
             if(number % num == 0):
                 flag = False
                 break
-            else:
-                primaries.append(number)
             
-        
+            
+        if(flag):
+           primaries.append(number) 
+           
+    set(primaries)
+    return sorted(primaries) 
+
+
+
+
+print(sieve_erathostens(150))
