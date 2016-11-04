89771d28cdd30ec25115e3d6aee492439f367988
yacoub <yacoubismus@googlemail.com>
2016-11-04 15:10:20 +0100

    third homework

---
+++ python-course/homework/third/Complement_dna.py
@@ -0,0 +1,11 @@
+'''
+Created on 04.11.2016
+
+@author: yacoub
+'''
+def complement_Dna(dna_input):
+    assert type(dna_input) is str, "input type is not string"
+    valid_alphabet = ['A','C','G','T']
+    input_as_list = list(dna_input)
+    if 
+    
