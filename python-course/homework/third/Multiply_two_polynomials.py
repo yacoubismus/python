64b47b2d9915ef25943e2de0003d48f4a13e9f69
Jacob <yacoubium@yahoo.de>
2016-11-06 18:21:56 +0100

    multiply 2 polynomials

---
+++ python-course/homework/third/Multiply_two_polynomials.py
@@ -0,0 +1,22 @@
+def multiply_poynomials(list1, list2):
+    result = []
+    lenght_list1 = len(list1)
+    lenght_list2 = len(list2)
+    short = []
+    big = []
+    if lenght_list1 == lenght_list2:
+        short = list1
+        big = list2
+    else:
+        short = (list2 if lenght_list1 - lenght_list2 > 0 else list1 )
+        big =(list1 if lenght_list1 - lenght_list2 > 0 else list2 )
+    for number in range(len(short)):
+        result.append(list1[number] + list2[number])
+
+    for number in range(len(short),len(big)):
+        result.append(big[number])
+    return result
+
+
+
+print (multiply_poynomials([1,2,4,5,6],[1,2,4,5,6]))
