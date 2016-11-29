e40a8b7daca65719b23640662ee66abfbaa9772d
yacoub <yacoubismus@googlemail.com>
2016-11-29 15:18:22 +0100

    new functions

--- python-course/homework/sixth/Base_conversion.py
+++ python-course/homework/sixth/Base_conversion.py
@@ -4,6 +4,7 @@ Created on 29.11.2016
 @author: yacoub
 '''
 import string
+from test.test_xml_etree import ElementSlicingTest
 
 CHAR_LOOKUP = list(string.digits + string.ascii_uppercase)
 
@@ -20,6 +21,27 @@ def convert_base(number, base):
         number //= base
     mods.reverse()
     return ''.join(mods)
+def base(n):  
+    bases_list = list(string.digits + string.ascii_uppercase)  # only 35 ElementSlicingTest 0 - 9 + A - Z
+    
+    #print(bases_list)
+    return bases_list[n]
+
+def base_convert(n,b):
+    modulus_list = []
+    assert type(b) is string , "Its not astring: %r" % b
+    if b < 2 or b > 36 :
+        return "invalid Base. Base should be bigger than 2 or smaller than 37"
+    while n > 0 :
+        
+        modulus = base(n % b)
+        modulus_list.append(modulus)
+        n = n / b
+    modulus_list = modulus_list[::-1]    
+    return modulus_list
+    
 
 if __name__ == "__main__": 
-    print(convert_base(11,16))
+    
+    print(base(9))
+    
