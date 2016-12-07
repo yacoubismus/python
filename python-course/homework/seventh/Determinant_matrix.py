e4e7abd77c578f1a16492902749f971f51d7c687
yacoub <yacoubismus@googlemail.com>
2016-12-07 09:02:34 +0100

    scalar

--- python-course/homework/seventh/Determinant_matrix.py
+++ python-course/homework/seventh/Determinant_matrix.py
@@ -1,31 +1,29 @@
-'''
-Created on 06.12.2016
-
-@author: yacoub
-'''
 import numpy as np
 from copy import copy, deepcopy
+det = 0
+def matrix_scalar_mul(a, c):
+
+   m = len(a)     # number of rows of A
+   n = len(a[0])  # number of cols of A
+   return [[c * a[i][j] for j in range(n)] for i in range(m)]
+
 def determinant_matrix(m):
-    det = 0
+    global det
     for x in list(range(len(m))):
-        det += m[0][x] * (-1)**(2+x) * determinant_matrix(matrix_get_submatrix(m,x,x))
-    
+        #det += m[0][x] * (-1) ** (2 + x) * determinant_matrix(matrix_get_submatrix(m, x, x))
+        det += (-1) * matrix_scalar_mul(determinant_matrix(matrix_get_submatrix(m, x, x)),m[0][x])
+        print("determinant:", det)
+    return det
 
-
-def matrix_get_submatrix(m,i,j):
+def matrix_get_submatrix(m, i, j):
     submat = deepcopy(m)
-    del submat[0] #Delete first row
-    for b in list(range(len(matrix))): #Delete column i
-        del submat[b][i]
-#     submat = np.delete(submat, i, axis=0)
-#     submat = np.delete(submat, j, axis= 1)
+    submat = np.delete(submat, i, axis=0)
+    submat = np.delete(submat, j, axis= 1)
     return submat
 
 
 if __name__ == "__main__":
-    a = np.array([[1,2,3],[4,5,6],[7,8,9]],dtype=int)
-    print(determinant_matrix(a))
-#     print(a)
-#     mm = matrix_get_submatrix(a,2,2)
-#     print(mm)
-#     print(a)  https://www.youtube.com/watch?v=JQ3AgsGEnKM
+     a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=int)
+     print(matrix_get_submatrix(a,0,1))
+     print(a)
+     determinant_matrix(a)
